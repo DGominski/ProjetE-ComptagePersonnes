449f82eafe83fd6de46fd90741bdbc92b1fde60f
Dimitri Gominski <dgominski@ge504-110-01.insa-lyon.fr>
2016-12-05 14:06:23 +0100

    test svm

--- Matlab/main.m
+++ Matlab/main.m
@@ -6,13 +6,14 @@ addpath('/img');
 
 %% ACQUISITION DE L'IMAGE
 
-script_1_load_data;
+img = imread('detection_0200.jpeg');
+img = rgb2gray(img);
 
 %% MOYENNAGE POUR ELIMINATION DE L'ARRIERE PLAN
 
-Ac = backgroundfilter(A);
-figure;
-histogram(Ac);
+% Ac = backgroundfilter(A);
+% figure;
+% histogram(Ac);
 
 %% DECOUPAGE DE L'IMAGE
 array = decoupe(img,50,80);
@@ -25,17 +26,19 @@ horz = [-1 1; -1 1];
 vert = [1 1 ; -1 -1];
 diag = [1 -1 ; -1 1];
 
+norm = size(array,1)*size(array,2);
 for i=1:nombre_de_fenetres_testees
     horzarray(:,:,i) = filter2(horz,array(:,:,i));
-    horzarray(:,:,i) = horzarray(:,:,i) > 0.5;
-    horzcount(i) = nnz(horzarray(:,:,i))/(size(horzarray,1)+size(horzarray,2));
+    temp = histogram(horzarray,5);
+    horzcount(:,i) = temp.Values/norm;
     vertarray(:,:,i) = filter2(vert,array(:,:,i));
-    vertarray(:,:,i) = vertarray(:,:,i) > 0.5;
-    vertcount(i) = nnz(vertarray(:,:,i))/(size(vertarray,1)+size(vertarray,2));
+    temp = histogram(vertarray,5);
+    vertcount(:,i) = temp.Values/norm;
     diagarray(:,:,i) = filter2(diag,array(:,:,i));
-    diagarray(:,:,i) = diagarray(:,:,i) > 0.5;
-    diagcount(i) = nnz(diagarray(:,:,i))/(size(diagarray,1)+size(diagarray,2));
+    temp = histogram(diagarray,5);
+    diagcount(:,i) = temp.Values/norm;
 end
+
 
 
 %% TRAINING SVM AVEC SET DE DONNEES DE REFERENCE
@@ -47,26 +50,27 @@ for i=1:10
 end
 
 imgref = cat(3,piet,fond);
+normref = size(piet,1)*size(piet,2);
 
 for i=1:20
     refhorzarray(:,:,i) = filter2(horz,imgref(:,:,i));
-    refhorzarray(:,:,i) = refhorzarray(:,:,i) > 0.8;
-    refhorzcount(i) = nnz(refhorzarray(:,:,i))/(size(refhorzarray,1)+size(refhorzarray,2));
+    temp = histogram(refhorzarray,5);
+    refhorzcount(:,i) = temp.Values/normref;
     refvertarray(:,:,i) = filter2(vert,imgref(:,:,i));
-    refvertarray(:,:,i) = refvertarray(:,:,i) > 0.8;
-    refvertcount(i) = nnz(refvertarray(:,:,i))/(size(refvertarray,1)+size(refvertarray,2));
+    temp = histogram(refvertarray,5);
+    refvertcount(:,i) = temp.Values/normref;
     refdiagarray(:,:,i) = filter2(diag,imgref(:,:,i));
-    refdiagarray(:,:,i) = refdiagarray(:,:,i) > 0.8;
-    refdiagcount(i) = nnz(refdiagarray(:,:,i))/(size(refdiagarray,1)+size(refdiagarray,2));
+    temp = histogram(refdiagarray,5);
+    refdiagcount(:,i) = temp.Values/normref;
 end
 
 
 refset = cat(1,refhorzcount,refvertcount,refdiagcount);
 group = [zeros(1,10) ones(1,10)];
-figure;
-svmStruct = svmtrain(refset',group','ShowPlot',true);
+svmStruct = svmtrain(refset',group');
 
 %% CLASSIFICATION
 
+n = randn(1:20
 set = cat(1,horzcount,vertcount,diagcount);
-result = svmclassify(svmStruct',set','ShowPlot',true);
+result = svmclassify(svmStruct',set');
