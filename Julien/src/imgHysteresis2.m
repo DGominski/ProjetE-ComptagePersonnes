30b4376ac401e2b64f66aaea18159c3a86fcb533
Julien Guichon <jguichon@ge504-110-15.insa-lyon.fr>
2016-12-05 12:13:25 +0100

    Ajout img pieton fond

--- Julien/src/imgHysteresis2.m
+++ Julien/src/imgHysteresis2.m
@@ -1,4 +1,4 @@
-function [imgSeuillee] = imgHysteresis( img )
+function [imgSeuillee] = imgHysteresis2( img )
 histogram(img);
 h = histogram(img);
 hData = h.Values;
@@ -8,12 +8,20 @@ for s=1:length(hData)
     J(s) =  (sum(n(1:s).*hData(1:s)))^2/sum(hData(1:s))+(sum(n(s+1:N).*hData(s+1:N)))^2/sum(hData(s+1:N));
 end
 
-[val threshold] = max(J);
-threshold
-imgSeuillee = img < threshold;
+[val threshold1] = max(J);
+threshold1
+[val indexMaxHist] = max(hData)
+threshold2 = indexMaxHist+(indexMaxHist-threshold1)
+
+threshold1 = indexMaxHist - 18;
+threshold2 = indexMaxHist + 18;
+
+imgSeuillee = (img > threshold2) + (threshold1 > img);
 subplot(1,3,1);plot(J);
 subplot(1,3,2);imagesc(imgSeuillee);colormap(gray);
 subplot(1,3,3);histogram(img);
 
 end
+
+
 
