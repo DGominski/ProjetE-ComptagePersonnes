80f6d3ca508842a82c3712d5a39b5c7300c0a0da
Julien Guichon <jguichon@ge504-110-14.insa-lyon.fr>
2016-12-09 12:30:20 +0100

    up

--- main.m
+++ main.m
@@ -5,11 +5,13 @@ clc;
 addpath('dataPieton');
 addpath('img');
 
-pietName = ['pieton_',num2str(1,'%0.2d'),'.jpeg'];
-fondName = ['fond_',num2str(1,'%0.2d'),'.jpeg'];
-
 %% 
-for n = 1:10
+N = 10;
+for n = 1:N
+    
+    pietName = ['pieton_',num2str(n,'%0.2d'),'.jpeg'];
+    fondName = ['fond_',num2str(n,'%0.2d'),'.jpeg'];
+
     pietData(n,:,:) = imread(pietName);
     fondData(n,:,:) = imread(fondName);
 end
@@ -17,9 +19,29 @@ end
 dataRef = [pietData fondData];
 classType = [ones(size(pietData,1),1);zeros(size(fondData,1),1)];
 
-%%
+%% Sobel
+Gx = [-1 0 1;
+     -2 0 2;
+     -1 0 1]
+Gy = Gx';
+H = 10;
+for i = 1:N
+    Gx_piet = filter2(Gx,squeeze(pietData(i,:,:)));
+    Gy_piet = filter2(Gy,squeeze(pietData(i,:,:)));
+    Gx_fond = filter2(Gx,squeeze(fondData(i,:,:)));
+    Gy_fond = filter2(Gy,squeeze(fondData(i,:,:)));
+    dataSobelPiet = squeeze(sqrt(Gy_piet.^2 + Gx_piet.^2));
+    dataSobelFond = squeeze(sqrt(Gy_fond.^2 + Gx_fond.^2));
+    hPiet = histogram(dataSobelPiet,H);
+    dataHistPiet{i} = hPiet.Values;
+    hFond = histogram(dataSobelFond,H);
+    dataHistFond{i} = hFond.Values;
+    close
+end
 
+dataHist = [dataHistPiet dataHistFond];
 
 %% 
+
 figure;
 svmStruct = svmtrain(dataRef,classType,'ShowPlot',true);
