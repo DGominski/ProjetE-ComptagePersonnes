6f43a82c50040ba6c99c8d9505797086b73bce10
Julien <julien.guichon@insa-lyon.fr>
2016-12-11 15:39:14 +0100

    HOG matlab 2016 -> ajuster les parametres

--- mainSVM_hog.m
+++ mainSVM_hog.m
@@ -12,15 +12,20 @@ addpath('hog_feature_vector');
 N = 20;
 Na = 20;
 Ntest = N-Na;
+
+HOG_cell = [8 8];
+
 for n = 1:N
     
     pietName = ['pieton_',num2str(n,'%0.2d'),'.jpeg'];
     fondName = ['fond_',num2str(n,'%0.2d'),'.jpeg'];
 
     data = double(imread(pietName));
-    pietData(n,:) = hog_feature_vector(data);
+    %pietData(n,:) = hog_feature_vector(data);
+    pietData(n,:) = extractHOGFeatures(data,'CellSize',HOG_cell);
     data = double(imread(fondName));
-    fondData(n,:) = hog_feature_vector(data);
+    %fondData(n,:) = hog_feature_vector(data);
+    fondData(n,:) = extractHOGFeatures(data,'CellSize',HOG_cell);
 end
 
 dataRef = [pietData(1:Na,:);fondData(1:Na,:)];
@@ -30,7 +35,7 @@ classType = [ones(size(pietData(1:Na,:),1),1);zeros(size(fondData(1:Na,:),1),1)]
 
 %% Apprentissage
 
-svmStruct = svmtrain(dataRef,classType,'kernel_function','quadratic');
+svmStruct = svmtrain(dataRef,classType,'kernel_function','linear');
 
 
 
@@ -48,24 +53,33 @@ addpath('src');
 imgName = ['detection_',num2str(200,'%0.4d'),'.jpeg'];
 img = rgb2gray(imread(imgName));
 
-wL = 100;
-wH = 40;
-dec = 1;
+wL = 40;
+wH = 100;
+dec = 10;
 imgDecoupe = decoupe(img,wL,wH,dec);
 
-
+chemin = 'D:\Documents\INSA LYON\2016-2017\Mini-projet-TDSI-Git\ProjetE-ComptagePersonnes\test\';
 
 for i = 1:size(imgDecoupe,3)
     i
-    hogData = hog_feature_vector(double(imgDecoupe(:,:,i)));
+    %hogData = hog_feature_vector(double(imgDecoupe(:,:,i)));
+    %imwrite(imgDecoupe(:,:,i),[chemin,'img_',num2str(i,'%0.6d'),'.jpeg']);
+    hogData = extractHOGFeatures(double(imgDecoupe(:,:,i)),'CellSize',HOG_cell);
     rep(i) = svmclassify(svmStruct,hogData);
+    if rep(i) == 1
+        imwrite(imgDecoupe(:,:,i),[chemin,'img_',num2str(i,'%0.6d'),'.jpeg']);
+    end
 
     close
 end
 
 %%
 
-posPieton = vec2mat(rep,(640-100)/dec);
+posPieton = vec2mat(rep,(640-40)/dec);
 
 subplot(1,2,1);imagesc(img);colormap(gray);axis equal
 subplot(1,2,2);imagesc(posPieton);colormap(gray);axis equal
+
+%%
+%subplot(1,2,1);imagesc(imread('pieton_01.jpeg'));colormap(gray);
+%subplot(1,2,2);[a,b] = extractHOGFeatures(imread('pieton_01.jpeg'),'CellSize',[3 3]);plot(b);
