754b43156bcdf514bdf08fe755ab506cb468e50b
DGominski <gominski.dimitri@gmail.com>
2016-12-13 21:21:35 +0100

    maj

--- Matlab/decoupe.m
+++ Matlab/decoupe.m
@@ -1,12 +1,29 @@
-function [ array ] = decoupe( A,n,m )
+function [ array ] = decoupe( A,wL,wH,step)
 %decoupe l'image en rectangles de nxm et renvoie un tableau contenant
-%toutes les images obtenues, decales de 1 pixel a chaque fois
-[k l] = size(A);
-array = zeros(n,m,k-n+l-m);
-for i=1:k-n
-    for j=1:l-m
-        array(:,:,i+j) = A(i:n+i-1,j:m+j-1);
+%toutes les images obtenues, decales de dec pixels a chaque fois
+[H L] = size(A);
+
+index = 1;
+for h = 1:step:H-wH
+    for l=1:step:L-wL
+        array(:,:,index) = A(h:(h+wH-1),l:(l+wL-1));
+        index = index + 1;
     end
 end
 
-    end
+end
+
+% function [ array ] = decoupe( A,n,m )
+% %decoupe l'image en rectangles de nxm et renvoie un tableau contenant
+% %toutes les images obtenues, decales de dec pixels a chaque fois
+% [k l] = size(A);
+% array = zeros(n,m,k-n+l-m);
+% index = 1;
+% for i=1:k-n
+%     for j=1:l-m
+%         index = index + j;
+%         array(:,:,index) = A(i:n+i-1,j:m+j-1);
+%     end
+% end
+% 
+% end
--- Matlab/main.m
+++ Matlab/main.m
@@ -2,7 +2,13 @@ clear all;
 close all;
 clc;
 
-addpath('/img');
+addpath('/img'); %données
+addpath('/data'); %set d'apprentissage
+
+
+%% TRAINING SVM
+
+
 
 %% ACQUISITION DE L'IMAGE
 
@@ -16,58 +22,13 @@ img = rgb2gray(img);
 % histogram(Ac);
 
 %% DECOUPAGE DE L'IMAGE
-array = decoupe(img,50,80);
+array = decoupe(img,40,100,10);
 nombre_de_fenetres_testees = size(array,3)
 figure;
 
 %% EXTRACTION DE DESCRIPTEURS LOCAUX SUR LES BLOCS
 
-horz = [-1 1; -1 1];
-vert = [1 1 ; -1 -1];
-diag = [1 -1 ; -1 1];
 
-norm = size(array,1)*size(array,2);
-for i=1:nombre_de_fenetres_testees
-    horzarray(:,:,i) = filter2(horz,array(:,:,i));
-    temp = histogram(horzarray,5);
-    horzcount(:,i) = temp.Values/norm;
-    vertarray(:,:,i) = filter2(vert,array(:,:,i));
-    temp = histogram(vertarray,5);
-    vertcount(:,i) = temp.Values/norm;
-    diagarray(:,:,i) = filter2(diag,array(:,:,i));
-    temp = histogram(diagarray,5);
-    diagcount(:,i) = temp.Values/norm;
-end
-
-
-
-%% TRAINING SVM AVEC SET DE DONNEES DE REFERENCE
-
-
-for i=1:10
-    piet(:,:,i) = imread(['pieton_',num2str(i,'%2.2u'),'.jpeg']);
-    fond(:,:,i) = imread(['fond_',num2str(i,'%2.2u'),'.jpeg']);
-end
-
-imgref = cat(3,piet,fond);
-normref = size(piet,1)*size(piet,2);
-
-for i=1:20
-    refhorzarray(:,:,i) = filter2(horz,imgref(:,:,i));
-    temp = histogram(refhorzarray,5);
-    refhorzcount(:,i) = temp.Values/normref;
-    refvertarray(:,:,i) = filter2(vert,imgref(:,:,i));
-    temp = histogram(refvertarray,5);
-    refvertcount(:,i) = temp.Values/normref;
-    refdiagarray(:,:,i) = filter2(diag,imgref(:,:,i));
-    temp = histogram(refdiagarray,5);
-    refdiagcount(:,i) = temp.Values/normref;
-end
-
-
-refset = cat(1,refhorzcount,refvertcount,refdiagcount);
-group = [zeros(1,10) ones(1,10)];
-svmStruct = svmtrain(refset',group');
 
 %% CLASSIFICATION
 
