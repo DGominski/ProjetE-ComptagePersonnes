869b4134bd68d51c5ab79bb90e1fbdb01feb9055
Julien <julien.guichon@insa-lyon.fr>
2017-01-07 11:41:18 +0100

    modif detection line

--- main.m
+++ main.m
@@ -1,3 +1,12 @@
 close all;
 clear all;
 clc;
+
+addpath('img');
+addpath('src');
+
+% Ligne de détection
+imgName = 'detection_0000.jpeg';
+img = rgb2gray(imread(imgName));
+[pts,vectIndexLigne] = setDetectionLine(img);
+
---
+++ src/setDetectionLine.m
@@ -0,0 +1,29 @@
+function [pts,vectIndexLigne] = setDetectionLine(img)
+
+figure;
+imagesc(img); colormap(gray); axis image;
+
+[x,y] = ginput(2)
+pt1 =[x(1) y(1)];
+pt2 =[x(2) y(2)];
+pts = [pt1;pt2]
+
+% Affichage des points
+hold on;
+plot(pt1(1),pt1(2),'og');
+plot(pt2(1),pt2(2),'og');
+plot([pt1(1) pt2(1)],[pt1(2) pt2(2)],'-g');
+
+% Calcul des index 
+v = pt2-pt1;
+deltaX = abs(pt2(1)-pt1(1));
+deltaY = abs(pt2(2)-pt1(2));
+p = round(sqrt(deltaX^2 + deltaY^2));
+
+for t=0:p
+    vectIndexLigne(t) = round(pt1 + v*t/p);
+end
+    
+
+end
+
